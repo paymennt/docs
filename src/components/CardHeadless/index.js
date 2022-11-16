0d927236b83c0a1a0ecc3beefb5c16e66a25b412
Bashar Saleh <bashar@paymennt.com>
2022-11-16 11:49:17 +0400

    Adding documentation to managing payment methods

--- src/components/CardHeadless/index.js
+++ src/components/CardHeadless/index.js
@@ -3,14 +3,19 @@ import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function CardHeadless({children, image, link, action}) {
+  let imageTag;
+  imageTag = <img
+    src={useBaseUrl(image)}
+    alt="{action}"
+    title="{action}"
+  />
+  if (link) {
+    imageTag = <a href={useBaseUrl(link)}>{imageTag}</a>
+  }
   return (
     <div className="card headless">
       <div className="card__image">
-        <img
-          src={useBaseUrl(image)}
-          alt="{action}"
-          title="{action}"
-        />
+        {imageTag}
       </div>
       <div className="card__body">
       {children}
