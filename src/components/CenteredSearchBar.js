f156b09ba4fd4a859dc678a8b93630b74f384f5c
Muhammad Bin Naeem <dashing_muhammad@live.com>
2024-09-21 23:46:14 +0500

    Similar search to one shown as an example implemented on the index page

--- src/components/CenteredSearchBar.js
+++ src/components/CenteredSearchBar.js
@@ -6,15 +6,17 @@ import './CenteredSearchBar.css'; // Add a CSS file for styling
 
 function CenteredSearchBar() {
   return (
+    
     <div className="centered-search-container">
       <DocSearch
         appId="LTT5YM30FP"
         apiKey="b65796a965c5dbfa70790646ec8f3dbd"
         indexName="paymennt"
-        placeholder="Search by name phone or email."
+        placeholder=""
       />
     </div>
   );
 }
 
 export default CenteredSearchBar;
+
